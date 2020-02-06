b66a908d006a410a29b0dbb8e4220d9854287562
barrbm <barrbm@MCD210-PC-17.uccc.uc.edu>
2020-02-06 11:38:49 -0500

    Commit the project, my lord

---
+++ Bla/src/bank/BrickandMortar.java
@@ -0,0 +1,23 @@
+package bank;
+
+public class BrickandMortar extends Bank {
+	
+	private String address;
+
+	public BrickandMortar() {
+	
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String toString()	{
+		return("");
+	}
+
+}
